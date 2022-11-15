4a08dfd8c3f48425a50aca0765379ba4af525cb0
Kurtmj93 <kurtmj93@gmail.com>
2022-11-15 14:10:17 -0500

    generatePassword function, button working

--- Develop/script.js
+++ Develop/script.js
@@ -5,26 +5,24 @@ const upperLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const numbers = '123456789';
 const specialChars = ' !"#$%&\'()*+,-./:;<=>?@[\\]^_\`{|}~';
 
-var charSet = lowerLetters + upperLetters + numbers + specialChars;
+let charSet = lowerLetters + upperLetters + numbers + specialChars;
 
 function randomIntWithin(min, max) {
   return Math.floor(Math.random() * (max-min + 1) + min)
 }
 
-var minChars = 8;
-var maxChars = 50;
+let minChars = 8;
+let maxChars = 50;
 
-var password = '';
-
-var i = randomIntWithin(minChars, maxChars);
-
-while(i > 0) {
-  password += charSet[Math.floor(Math.random() * charSet.length)];
-  i--;
+function generatePassword() {
+  let password = '';
+  let i = randomIntWithin(minChars, maxChars);
+  while(i > 0) {
+    password += charSet[Math.floor(Math.random() * charSet.length)];
+    i--;
+  }
+  return password;
 }
-
-console.log(password);
-
 
 
 // Get references to the #generate element
@@ -32,7 +30,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  let password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
