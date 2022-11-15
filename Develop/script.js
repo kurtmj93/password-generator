7c1b86bb62687473bf68e73774771de5aa9108b0
Kurtmj93 <kurtmj93@gmail.com>
2022-11-15 15:42:24 -0500

    password generation working in prompts

--- Develop/script.js
+++ Develop/script.js
@@ -5,23 +5,58 @@ const upperLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const numbers = '123456789';
 const specialChars = ' !"#$%&\'()*+,-./:;<=>?@[\\]^_\`{|}~';
 
-let charSet = lowerLetters + upperLetters + numbers + specialChars;
-
 function randomIntWithin(min, max) {
   return Math.floor(Math.random() * (max-min + 1) + min)
 }
 
-let minChars = 8;
-let maxChars = 50;
+var minChars = 8;
+var maxChars = 128;
 
 function generatePassword() {
+  let charSet = lowerLetters;
   let password = '';
-  let i = randomIntWithin(minChars, maxChars);
-  while(i > 0) {
-    password += charSet[Math.floor(Math.random() * charSet.length)];
-    i--;
+  let uppersOK = confirm('Do you want to include uppercase letters?');
+  if (uppersOK) {
+    charSet += upperLetters;
+  } else {
+    alert("We won't use uppercase letters.");
   }
-  return password;
+  let numbersOK = confirm('Do you want to include numbers?');
+  if (numbersOK) {
+    charSet += numbers;
+  } else {
+    alert("We won't use numbers.");
+  }
+  let specialOK = confirm('Do you want to include special characters?');
+  if (specialOK) {
+    charSet += specialChars;
+  } else {
+    alert("We won't use special characters.")
+  }
+//  console.log(charSet); // checking to see if charSet worked as intended.
+  let randomOK = confirm('Would you like a password of random length between 8 and 128 characters?')
+  if (randomOK) {
+    let i = randomIntWithin(minChars, maxChars);
+    while (i > 0) {
+      password += charSet[Math.floor(Math.random() * charSet.length)];
+      i--;
+    }
+    return password;
+  } else {
+    let i = prompt("How long would you like your password to be?", 8);
+    if (i > 7 && i < 129) {
+      while (i > 0) {
+        password += charSet[Math.floor(Math.random() * charSet.length)];
+        i--;
+      }
+      return password;
+    }  
+      else {
+        alert('Please enter an integer between 8 and 128.');
+        password = 'Please try again.';
+        return password;
+      }
+  }
 }
 
 
